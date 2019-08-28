458ca661f9b796dac930310e27e949eb5b7696a7
Dan Hogan <hogan.daniel@hotmail.com>
2019-08-28 09:23:29 -0700

    Create react routes in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import TopWords from '../containers/words/TopWords';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/words/:count" component={TopWords} />
+        <Route exact path="/" component={TopWords} />
+      </Switch>
+    </Router>
+  );
 }
-  
